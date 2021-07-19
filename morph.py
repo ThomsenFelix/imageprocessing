18f00c0e2e98e7cf8b83befdf277a192c42fe967
Agustin C <agustin.copita@uns.edu.ar>
2021-07-19 14:04:08 -0300

    Update for gray scale morph optimization
    There's a lot of work to apply a morphological operation to a colored imagen but not the same for gray scale and even less when using a box structure element, so here's my update!

--- morph.py
+++ morph.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import tkinter as tk
+import scipy.ndimage as sp
 
 name = 'Morphology'
     
@@ -20,7 +21,7 @@ class Morph(filters.im_user_filter):
         super().__init__(image, parent)
         
     def _create_frame(self):
-        a = ['Erode','Dilate','Median','Ext_Borders','Int_Borders','Gradient','TopHat','BottomHat','Open','Close','OC','CO']
+        a = ['Erode','Dilate','Ext_Borders','Int_Borders','Gradient','TopHat','BottomHat','Open','Close','OC','CO']
         self.values = sorted(a)    
         self.tools.config(text="Change Settings")        
         self.cb_item = tk.StringVar()
@@ -44,14 +45,53 @@ class Morph(filters.im_user_filter):
         self.cb_item2.set(self.values[0])
         self.scaleperc.set(1)
         
-    def do(self):       
-        def _morph_gray(im, se, op):
-            result = np.zeros(im.shape)
-            offset = (np.array(se.shape)-1)//2
-            im = np.pad(im,[(offset[0],offset[0]),(offset[1],offset[1])],'edge')
-            for y, x in np.ndindex(result.shape):
-                pixels = im[y: y + se.shape[0], x: x + se.shape[1]][se]
-                result[y, x] = op(pixels)
+    def do(self):  
+#-------------------------------Dilate & Erode---------------------------------
+        def _morph_erode(im, value2, r, gray):
+            if is_gray:
+                im = im.mean(axis=2)
+                if value2 == 'Box':
+                    #Box it's easier to process than other structures
+                    result = sp.grey_erosion(im,size=(r,r))
+                elif value2 == 'Circle':
+                    threshold=0.5
+                    vec = np.linspace(-r, r, r*2+1)
+                    [x,y] = np.meshgrid(vec,vec) 
+                    se = (x**2 + y**2)**0.5 < (r + threshold)
+                    result = sp.grey_erosion(im,footprint=se,size=(r,r))
+                result = result[:, :, np.newaxis].repeat(3,2)
+            else:
+                if value2 == 'Box':
+                    se = np.ones((r*2+1,r*2+1),dtype=np.bool)
+                elif value2 == 'Circle':
+                    threshold=0.5
+                    vec = np.linspace(-r, r, r*2+1)
+                    [x,y] = np.meshgrid(vec,vec) 
+                    se = (x**2 + y**2)**0.5 < (r + threshold)
+                result = _morph_color(im,se,np.argmin)
+            return result
+        
+        def _morph_dilation(im, value2, r, gray):
+            if is_gray:
+                im = im.mean(axis=2)
+                if value2 == 'Box':
+                    result = sp.grey_dilation(im,size=(r,r))
+                elif value2 == 'Circle':
+                    threshold=0.5
+                    vec = np.linspace(-r, r, r*2+1)
+                    [x,y] = np.meshgrid(vec,vec) 
+                    se = (x**2 + y**2)**0.5 < (r + threshold)
+                    result = sp.grey_dilation(im,footprint=se,size=(r,r))
+                result = result[:, :, np.newaxis].repeat(3,2)
+            else:
+                if value2 == 'Box':
+                    se = np.ones((r*2+1,r*2+1),dtype=np.bool)
+                elif value2 == 'Circle':
+                    threshold=0.5
+                    vec = np.linspace(-r, r, r*2+1)
+                    [x,y] = np.meshgrid(vec,vec) 
+                    se = (x**2 + y**2)**0.5 < (r + threshold)
+                result = _morph_color(im,se,np.argmax)
             return result
         
         def _morph_color(im, se, op):
@@ -68,50 +108,42 @@ class Morph(filters.im_user_filter):
             result = _morph_multiband(im2, se, op)[:, :, 1:]
             return result
                 
-
+        data = self.image
         value = self.cb_item.get()
         value2 =  self.cb_item2.get()
         r = int(self.scaleperc.get())
+        ep = 10**-5
+        is_gray = data.var(axis=2).mean() < ep
         
-#-------------------------------SE options---------------------------------
-        if value2 == 'Box':
-            se = np.ones((r*2+1,r*2+1),dtype=np.bool)
-        elif value2 == 'Circle':
-            threshold=0.5
-            vec = np.linspace(-r, r, r*2+1)
-            [x,y] = np.meshgrid(vec,vec) 
-            se = (x**2 + y**2)**0.5 < (r + threshold)
 #----------------------------Operation----------------------------
-        data = self.image
-        if value=='Erode':
-            op=np.argmin
-            if len(data.shape)==2: op=np.min
-        elif value=='Dilate':
-            op=np.argmax
-            if len(data.shape)==2: op=np.max
-        elif value=='Median':
-            op= lambda px: np.argsort(px)[len(px)//2]
-            if len(data.shape)==2: op=np.median
-        elif value=='Ext_Borders':
-            return _morph_color(data,se,np.argmax)[:,:,:3] - data
-        elif value=='Int_Borders':
-            return data - _morph_color(data,se,np.argmin)[:,:,:3] 
-        elif value=='Gradient':
-            return _morph_color(data,se,np.argmax)[:,:,:3] - _morph_color(data,se,np.argmin)[:,:,:3] 
-        elif value=='Open':
-            return _morph_color(_morph_color(data,se,np.argmin)[:,:,:3],se,np.argmax)[:,:,:3]
-        elif value=='Close':
-            return _morph_color(_morph_color(data,se,np.argmax)[:,:,:3],se,np.argmin)[:,:,:3]
-        elif value=='BottomHat':
-            return _morph_color(_morph_color(data,se,np.argmax)[:,:,:3],se,np.argmin)[:,:,:3] - data
-        elif value=='TopHat':
-            return data - _morph_color(_morph_color(data,se,np.argmin)[:,:,:3],se,np.argmax)[:,:,:3]
-        elif value=='OC':
-            return _morph_color(_morph_color(_morph_color(_morph_color(data,se,np.argmax)[:,:,:3],se,np.argmin)[:,:,:3],se,np.argmin)[:,:,:3],se,np.argmax)[:,:,:3]
-        elif value=='CO':
-            return _morph_color(_morph_color( _morph_color(_morph_color(data,se,np.argmin)[:,:,:3],se,np.argmax)[:,:,:3],se,np.argmax)[:,:,:3],se,np.argmin)[:,:,:3]
         
-        if len(self.image.shape)==2:
+        if value=='Erode':
+            #np.argmin
+            return _morph_erode(data, value2, r, is_gray)
+        elif value=='Dilate':
+            #np.argmax
+            return _morph_dilation(data, value2, r, is_gray)
+        elif value=='Ext_Borders':
+            return _morph_dilation(data, value2, r, is_gray) - data
+        elif value=='Int_Borders':
+            return data - _morph_erode(data, value2, r, is_gray)
+        elif value=='Gradient':
+            return _morph_dilation(data, value2, r, is_gray) - _morph_erode(data, value2, r, is_gray)
+        elif value=='Open':
+            return _morph_dilation(_morph_erode(data, value2, r, is_gray),value2, r, is_gray)
+        elif value=='Close':
+            return _morph_erode(_morph_dilation(data, value2, r, is_gray),value2, r, is_gray)
+        elif value=='BottomHat':
+            return _morph_erode(_morph_dilation(data, value2, r, is_gray),value2, r, is_gray) - data
+        elif value=='TopHat':
+            return data - _morph_dilation(_morph_erode(data, value2, r, is_gray),value2, r, is_gray)
+        elif value=='OC':
+            return _morph_erode(_morph_dilation(_morph_dilation(_morph_erode(data, value2, r, is_gray),value2, r, is_gray), value2, r, is_gray),value2, r, is_gray)
+        elif value=='CO':
+            return _morph_dilation(_morph_erode(_morph_erode(_morph_dilation(data, value2, r, is_gray),value2, r, is_gray), value2, r, is_gray),value2, r, is_gray)
+
+        
+        if is_gray:
             data = self.image
             return filters.yiq2rgb(_morph_gray(data,se,op))
         else:
